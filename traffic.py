e9c060f70500872625d2d71d161b4dc7953567d0
Maksim Artemev <dan-pro352@ya.ru>
2017-12-16 22:51:24 +0300

    Dorabotani try/except dlia img

--- traffic.py
+++ traffic.py
@@ -2,17 +2,20 @@ import requests
 import datetime
 
 def GetTraffic():
-    response = requests.get('http://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&spn=0.1,0.1&l=map,trf')
-    now = datetime.datetime.now()
-    filename = "traffic_img/%s.png" % now.strftime("%H-%M-%S--%d-%m-%Y")
-    out = open(filename, "wb")
-    out.write(response.content)
-    out.close()
+    try:
+        response = requests.get('http://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&spn=0.1,0.1&l=map,trf')
+        now = datetime.datetime.now()
+        filename = "traffic_img/%s.png" % now.strftime("%H-%M-%S--%d-%m-%Y")
+        out = open(filename, "wb")
+        out.write(response.content)
+        out.close()
 
-    traffic_img_name = now.strftime("%H-%M-%S--%d-%m-%Y") + ".png"
-    return(traffic_img_name)
+        traffic_img_name = now.strftime("%H-%M-%S--%d-%m-%Y") + ".png"
+        return traffic_img_name
+        print('Изображение успешно сохранено в папке')
+    except requests.exceptions.RequestException:
+        return 'Возникла ошибка подключения к API Яндекс.Карт'
+'''Вызов функции получения имени изображения GetTraffic()'''
 
 def GetNumTraffic():
     pass
-'''Имя фотографии передается в переменную traffic_img_name'''
-'''Для вызова функции вызови GetTraffic()'''
