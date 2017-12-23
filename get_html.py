6b7109188a001e60c7d209811bd313f5e42fc44e
caradvisor@mail.com <@mail>
2017-12-23 20:59:04 +0300

    Added get_html.py

---
+++ get_html.py
@@ -0,0 +1,17 @@
+import requests
+
+
+url = 'https://yandex.ru/search/?text=python&lr=213'
+
+
+def get_html(url):
+
+    try:
+        result = requests.get(url)
+        return result.text
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return False
+
+
