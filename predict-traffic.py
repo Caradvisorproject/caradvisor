ef1b6922777b483a879213d737f555b00939cb39
Maksim Artemev <dan-pro352@ya.ru>
2018-01-25 00:55:39 +0300

    last function

--- predict-traffic.py
+++ predict-traffic.py
@@ -2,38 +2,23 @@ import requests
 from lxml import etree
 from datetime import datetime
 
-def predict_points():
-    curr_hour = datetime.now().strftime('%H')
-    curr_hour = datetime.now().strftime('%H')
+from datetime import datetime, timedelta
+
+def predict_function(predict_list, predict_start, predict_end, predict_step):
+    predict_traff_stats = predict_list
     curr_weekday = datetime.now().weekday()
-    weekdays = {0:'mon', 1:'tue', 2:'wed', 3:'thu', 4:'fri', 5:'sat', 6:'sun'}
-    predict_traff = {0: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
-    1: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
-    2: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
-    3: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
-    4: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
-    5: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1, 8:2, 9:3, 10:3, 11:4, 12:5, 13:6, 14:5, 15:6, 16:7, 17:6, 18:7, 19:6, 20:5, 21:4, 22:2, 23:0}, 
-    6: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1, 8:2, 9:3, 10:3, 11:4, 12:5, 13:6, 14:5, 15:6, 16:7, 17:6, 18:7, 19:6, 20:5, 21:4, 22:2, 23:0}}
-    curr_hour = int(curr_hour)
     curr_weekday = int(curr_weekday)
-    list_points = []
-    if curr_hour == 21:
-        list_points.append(int(predict_traff[curr_weekday][22]))
-        list_points.append(int(predict_traff[curr_weekday][1]))
-        list_points.append(int(predict_traff[curr_weekday][4]))
-    elif curr_hour == 22:
-        list_points.append(int(predict_traff[curr_weekday][1]))
-        list_points.append(int(predict_traff[curr_weekday][4]))
-        list_points.append(int(predict_traff[curr_weekday][7]))
-    elif curr_hour == 23:
-        list_points.append(int(predict_traff[curr_weekday][2]))
-        list_points.append(int(predict_traff[curr_weekday][5]))
-        list_points.append(int(predict_traff[curr_weekday][8]))
-    else:
-        list_points.append(int(predict_traff[curr_weekday][curr_hour+3]))
-        list_points.append(int(predict_traff[curr_weekday][curr_hour+6]))
-        list_points.append(int(predict_traff[curr_weekday][curr_hour+9]))
-    return list_points
+    curr_hours = datetime.now().hour
+    
+    hour_list = []
+    for hours in range(predict_start, predict_end, predict_step):
+        append_hour = datetime.now() + timedelta(hours=hours)
+        hour_list.append(append_hour.hour)
+    
+    predict_list = []
+    for predict in hour_list:
+        predict_list.append(predict_traff_stats[curr_weekday][predict])
+    return predict_list
 
 url = 'https://export.yandex.ru/bar/reginfo.xml?region=213'
 result = requests.get(url)
@@ -65,9 +50,21 @@ def day_type():
 
 if __name__ == "__main__":
     jams_now = get_jams() + ': ' + get_level() + ' (' + get_icon_jams() + ') '
-    jams_predict = 'Предстоящие баллы пробок: ' + str(predict_points())
     day_type = day_type()
     
     print(day_type)
     print(jams_now)
-    print(jams_predict)
+
+    predict_start, predict_end, predict_step = 3, 12, 3
+    predict_stat = {
+        0: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
+        1: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
+        2: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
+        3: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
+        4: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:1, 7:2, 8:5, 9:7, 10:8, 11:7, 12:5, 13:4, 14:4, 15:5, 16:5, 17:6, 18:7, 19:7, 20:5, 21:4, 22:2, 23:0}, 
+        5: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1, 8:2, 9:3, 10:3, 11:4, 12:5, 13:6, 14:5, 15:6, 16:7, 17:6, 18:7, 19:6, 20:5, 21:4, 22:2, 23:0}, 
+        6: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:1, 8:2, 9:3, 10:3, 11:4, 12:5, 13:6, 14:5, 15:6, 16:7, 17:6, 18:7, 19:6, 20:5, 21:4, 22:2, 23:0}
+        }
+    view_predict = predict_function(predict_stat, predict_start, predict_end, predict_step)
+    
+    print(view_predict)
